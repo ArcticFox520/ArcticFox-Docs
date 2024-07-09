59df124181142e00a488d50bb8a06a48b2d18b94
ArcticFox520 <ArcticFox520@qq.com>
2024-07-09 22:17:57 +0800

    测试提交

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -49,10 +49,10 @@ export default hopeTheme({
     // 注意: 仅用于测试! 你必须自行生成并在生产环境中使用自己的评论服务
     comment: {
       provider: "Giscus",
-      repo: "vuepress-theme-hope/giscus-discussions",
-      repoId: "R_kgDOG_Pt2A",
+      repo: "https://github.com/ArcticFox520/ArcticFox-Docs",
+      repoId: "R_kgDOMT956A",
       category: "Announcements",
-      categoryId: "DIC_kwDOG_Pt2M4COD69",
+      categoryId: "DIC_kwDOMT956M4Cgq5U",
     },
 
     components: {
